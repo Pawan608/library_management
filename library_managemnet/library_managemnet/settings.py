eb24863b2368da37579492d3a3868793d7312a5e
Pawan608 <agarwalpawan6089@gmail.com>
2023-08-15 17:12:06 +0530

    vercel

--- library_managemnet/library_managemnet/settings.py
+++ library_managemnet/library_managemnet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2vuncj1j#*byu@&n^+575h3yn@y=q+emkk5g!upt5j6i5!#s*w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -126,9 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = '/static/' 
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticFiles_build','static')
